7f732f25db9988871b78d253718c7d65c50d7f2d
Mark Andrews <mjandrews.org@gmail.com>
2020-11-26 21:23:16 +0000

    Update recordings and scripts

--- recordings.md
+++ recordings.md
@@ -11,5 +11,5 @@ These are password protected, and you will have been given this password in the 
 
 * [Session 1: 12pm to 2pm](https://vimeo.com/484071244)
 * [Session 2: 3pm to 5pm](https://vimeo.com/484142663)
-* Session 3: 6pm to 8pm
+* [Session 3: 6pm to 8pm](https://vimeo.com/484205760)
 
--- scripts/script2.R
+++ scripts/script2.R
@@ -14,27 +14,63 @@ ggplot(classroom_df,
 M13 <- lme4::lmer(mathscore ~ 1 + ses + (1 + ses | schoolid) + (1 + ses | classid),
                   data = classroom_df)
 
-
 M13a <- lme4::lmer(mathscore ~ 1 + ses + (1 + ses | schoolid) + (1 + ses || classid),
                    data = classroom_df)
 
-
-# M13b and M13c are the same
+# 
 M13b <- lme4::lmer(mathscore ~ 1 + ses + (1 + ses | schoolid) + (1 | classid),
                    data = classroom_df)
 
-M13c <- lme4::lmer(mathscore ~ 1 + ses + (1  + ses | schoolid) + (1|schoolid:classid2),
+M13c <- lme4::lmer(mathscore ~ 1 + ses + (1 + ses | schoolid) + (1 | schoolid:classid2),
                    data = classroom_df)
 
-# M14a and M14b and M14c are the same
+# M14a and M14b are the same
 M14a <- lme4::lmer(mathscore ~ 1 + ses + (1 | schoolid) + (1|classid),
                    data = classroom_df)
 
 M14b <- lme4::lmer(mathscore ~ 1 + ses + (1 | schoolid/classid2),
                    data = classroom_df)
 
-M14c <- lme4::lmer(mathscore ~ 1 + ses + (1 | schoolid) + (1|schoolid:classid2),
+M14c <- lme4::lmer(mathscore ~ 1 + ses + (1|schoolid) + (1 | schoolid:classid2),
                    data = classroom_df)
 
 
+# lm(y ~ x * z)
+# lm(y ~ x + z + x:z)
 
+# crossed structures ------------------------------------------------------
+
+blp_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/immr03/master/data/blp-short2.csv")
+
+M15 <- lmer(rt ~ 1 + (1|spelling)  + (1|participant), data = blp_df)
+
+
+# group level predictors ---------------------------------------------------
+
+mathachschool_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/immr03/master/data/mathachieveschool.csv")
+mathach_df <- read_csv("https://raw.githubusercontent.com/mark-andrews/immr03/master/data/mathachieve.csv")
+mathach2_df <- inner_join(mathachschool_df, mathach_df, by = 'school')
+
+M16 <- lmer(mathach ~ ses + himinty + ses:himinty + (1 + ses | school),
+            data = mathach2_df)
+
+
+# generalized linear models -----------------------------------------------
+
+sleepstudy2 <- mutate(sleepstudy, fast_rt = Reaction < median(Reaction))
+
+M17 <- glmer(fast_rt ~ 1 + Days + (1 + Days | Subject),
+             data = sleepstudy2,
+             family = binomial(link = 'logit'))
+
+
+
+# brms bayesian sleepstudy ------------------------------------------------
+
+M8 <- lmer(Reaction ~ Days + (Days | Subject),
+           data = sleepstudy)
+
+library(brms)
+
+M18 <- brm(Reaction ~ Days + (Days | Subject),
+           data = sleepstudy)
